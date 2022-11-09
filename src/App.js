6b315fed9f2e6299048348b475bf949be91dec0b
JaeWon Baek <bjw1622@gmail.com>
2022-11-09 09:59:18 +0900

    redux-saga

--- src/App.js
+++ src/App.js
@@ -2,37 +2,47 @@ import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import Counter from "./components/counter/Counter";
-import { counterReducer } from "./components/counter/Slice";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TodoList from "./components/TodoList";
-import { todoList } from "./components/todoList/TodoListSlice";
 import CalendarUI from "./components/calendar/CalendarUI";
-import { CalendarSliceReducer } from "./components/calendar/CalendarSlice";
 import Layout from "./components/Layout";
-import { boardReducer } from "./components/board/BoardSlice";
 import Index from "./components/board/Index";
 import Write from "./components/board/Write";
+import TodoList from "./components/TodoList";
+import createSagaMiddleware from "redux-saga";
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./rootReducer";
+import { watchCounter } from "./components/counter/saga";
+// import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
+import { all, fork, takeLatest, delay, put } from "redux-saga/effects";
 
-const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    addList: todoList,
-    calendarReducer: CalendarSliceReducer,
-    boardReducer: boardReducer,
-  },
-});
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만듭니다.
+
+function* rootSaga() {
+  yield all([watchCounter()]);
+}
+
+const store = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+    devTools: true,
+    middleware: [sagaMiddleware],
+  });
+
+  sagaMiddleware.run(rootSaga);
+  return store;
+};
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <Provider store={store()}>
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/count" element={<Counter />}></Route>
-            <Route path="/" element={<TodoList />}></Route>
+            {/* <Route path="/" element={<TodoList />}></Route>
             <Route path="/calendar" element={<CalendarUI />}></Route>
             <Route path="/board" element={<Index />}></Route>
-            <Route path="/board/write" element={<Write />}></Route>
+            <Route path="/board/write" element={<Write />}></Route> */}
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,53 +1,15 @@
 import { counterAction } from "./Slice";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import React, { useEffect } from "react";
-import Axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import moment from "moment";
-import { todoListAction } from "../todoList/TodoListSlice";
+import React from "react";
 
 const CounterBtn = styled.button`
    {
     opacity: ${(count) => (count === 2 ? "0.1" : "1")};
   }
 `;
-let inputCount = 1;
 const Counter = () => {
   const dispatch = useDispatch();
-
-  const addData = {
-    addDate: moment(new Date()).format("DD-MM-YYYY"),
-    check: false,
-    id: uuidv4(),
-    inputValue: `test${inputCount++}`,
-  };
-
-  const totalList = useSelector((state) => {
-    return state.addList;
-  });
-
-  // Axios get방식
-  useEffect(() => {
-    async function getData() {
-      const value = await Axios.get("http://localhost:3001/todo");
-      console.log(value);
-      console.log(totalList);
-      console.log("useEffect 실행");
-    }
-    getData();
-  }, [totalList]);
-
-  // Axios post방식
-  const btnClick = () => {
-    Axios.post(`http://localhost:3001/todo`, addData)
-      .then((res) => {
-        dispatch(todoListAction.add(addData));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
   const count = useSelector((state) => {
     return state.counter.value;
@@ -56,14 +18,15 @@ const Counter = () => {
   const incremntEvent = () => {
     dispatch(counterAction.up(2));
   };
+
   return (
     <>
       <div>
         <br />
         <CounterBtn onClick={incremntEvent}>+</CounterBtn> {count}
       </div>
-      <button onClick={btnClick}>post 테스트</button>
     </>
   );
 };
+
 export default Counter;
