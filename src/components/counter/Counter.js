80182e0de5dd8eb65149eab83493604e36ca1280
JaeWon Baek <bjw1622@gmail.com>
2022-11-09 17:36:16 +0900

    counst add redux-saga

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -8,17 +8,22 @@ const CounterBtn = styled.button`
     opacity: ${(count) => (count === 2 ? "0.1" : "1")};
   }
 `;
+
 const Counter = () => {
   const dispatch = useDispatch();
   const count = useSelector((state) => {
     return state.counter.value;
   });
+
   useEffect(() => {
     dispatch(counterAction.getCounstRequest());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   const incremntEvent = () => {
     dispatch(counterAction.upRequest());
   };
+
   return (
     <>
       <div>
